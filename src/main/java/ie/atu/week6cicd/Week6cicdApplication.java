c5f6f79b984ced2150e6ad9d2ca3f90f4c251366
rory619 <g00422319@atu.ie>
2024-10-22 10:02:39 +0100

    starting week5 lab

---
+++ src/main/java/ie/atu/week6cicd/Week6cicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week6cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6cicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week6cicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week6cicd/Week6cicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week6cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week6cicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
